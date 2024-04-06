a38bcc4b7cb3ad2645e303e16dfba91acd7c9a3e
XKEYSCORE1 <milanfusco68@gmail.com>
2024-04-06 17:06:03 -0400

    Modified page.tsx and layout.tsx to align with our project

--- tekvis/app/layout.tsx
+++ tekvis/app/layout.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Head from 'next/head';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import React from "react";
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 const metadata = {
-  title: 'Tekken 8 Data Visualizer',
-  description: 'Visualize your Tekken 8 stats and improve your gameplay!',
+  title: "Tekken 8 Data Visualizer",
+  description: "Visualize your Tekken 8 stats and improve your gameplay!",
 };
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
@@ -23,9 +23,7 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </>
   );
 };
